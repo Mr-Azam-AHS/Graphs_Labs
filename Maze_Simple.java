c43a71a255bddc81cf9697d2d016422b9cb044db
Mr-Azam-AHS <156012568+Mr-Azam-AHS@users.noreply.github.com>
2026-01-12 10:30:13 -0600

    Add files via upload

---
+++ Maze_Simple.java
@@ -0,0 +1,30 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Maze_Simple 
+{
+	private static char[][]mat;
+	private static boolean[][]smat;
+
+	public static void solve(int r, int c) 
+	{
+		//add in your base case
+			//add in your recursive calls
+	}
+	
+	public static void main(String[]args) throws Exception
+	{
+		Scanner file=new Scanner(new File("maze_simple.dat"));
+		
+		
+		//read in the maze data
+		
+		//call the solve method
+		
+		//print out some stuff
+	}	
+}
